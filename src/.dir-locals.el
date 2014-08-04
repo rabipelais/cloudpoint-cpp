766f2022aef4e0365ff90a758d82dfb7b3f3c275
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-08-04 19:57:09 +0200

    Changed emacs compile command.

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -1,2 +1,2 @@
-((c++-mode . ((compile-command . "make -C ../build -j 4")))
- ((c-mode . ((compile-command . "make -C ../build -j 4")))))
+((c++-mode . ((compile-command . "make -C ../build -j 8")))
+ ((c-mode . ((compile-command . "make -C ../build -j 8")))))
