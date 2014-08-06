e7597be7968ca26dc5c16a9fc4e4083d24ae4992
Sebastian Mendez <sebas.chinoir@gmail.com>
2014-08-06 02:25:14 +0200

    Added distance to line and trig

--- src/point.hpp
+++ src/point.hpp
@@ -77,4 +77,192 @@ double angle(const Point& a, const Point& b, const Point& c) {
 	return std::max(0.0, acos(prod / (d1 * d2)));
 }
 
+/*
+ * Returns the distance from the point p to the line defined by xy.
+ */
+template<class Point>
+double distanceToLine(const Point & x, const Point & y, const Point & p) {
+	// d^2 = (|x - p|^2 * |y - x|^2 - ((x - p) * (y - x))^2) / |y - x|^2
+	Point px = sub(x, p);
+	Point xy = sub(y, x);
+	double nom = pow(norm(px), 2) * pow(norm(xy), 2) - pow(dot(px, xy), 2);
+	double d = nom / pow(norm(xy), 2);
+	return sqrt(d);
+}
+
+
+/*
+ * Returns the distance from the point p to the CCW triangle defined by xyz.
+ */
+template<class Point>
+double distanceToTriangle(const Point & x, const Point & y,
+                          const Point & z, const Point & p)
+{
+	Point diff = sub(x, p);
+	Point edge0 = sub(y, x);
+	Point edge1 = sub(z, x);
+	double a00 = pow(norm(edge0), 2);
+	double a01 = dot(edge0, edge1);
+	double a11 = pow(norm(edge1), 2);
+	double b0 = dot(diff, edge0);
+	double b1 = dot(diff, edge1);
+	double c = pow(norm(diff), 2);
+	double det = abs(a00 * a11 - a01 * a01);
+	double s = a01 * b1 - a11 * b0;
+	double t = a01 * b0 - a00 * b1;
+	double sqrDistance;
+
+	if (s + t <= det) {
+		if (s < 0) {
+			if (t < 0) {	//region 4
+				if (b0 < 0) {
+					t = 0;
+					if (-b0 >= a00) {
+						s = 1;
+						sqrDistance = a00 + (2) * b0 + c;
+					} else {
+						s = -b0 / a00;
+						sqrDistance = b0 * s + c;
+					}
+				} else {
+					s = 0;
+					if (b1 >= 0) {
+						t = 0;
+						sqrDistance = c;
+					} else if (-b1 >= a11) {
+						t = 1;
+						sqrDistance = a11 + (2) * b1 + c;
+					} else {
+						t = -b1 / a11;
+						sqrDistance = b1 * t + c;
+					}
+				}
+			} else {		//region 3
+				s = 0;
+				if (b1 >= 0) {
+					t = 0;
+					sqrDistance = c;
+				} else if (-b1 >= a11) {
+					t = 1;
+					sqrDistance = a11 + (2) * b1 + c;
+				} else {
+					t = -b1 / a11;
+					sqrDistance = b1 * t + c;
+				}
+			}
+		} else if (t < 0) {	// region 5
+			t = 0;
+			if (b0 >= 0) {
+				s = 0;
+				sqrDistance = c;
+			} else if (-b0 >= a00) {
+				s = 1;
+				sqrDistance = a00 + (2) * b0 + c;
+			} else {
+				s = -b0 / a00;
+				sqrDistance = b0 * s + c;
+			}
+		} else {		//region 0
+			// minimum at interior point
+			double invDet = (1) / det;
+			s *= invDet;
+			t *= invDet;
+			sqrDistance = s * (a00 * s + a01 * t + (2) * b0) +
+				t * (a01 * s + a11 * t + (2) * b1) + c;
+		}
+	} else {
+		double tmp0, tmp1, numer, denom;
+		if (s < 0) {			//region 2
+			tmp0 = a01 + b0;
+			tmp1 = a11 + b1;
+			if (tmp1 > tmp0) {
+				numer = tmp1 - tmp0;
+				denom = a00 - (2) * a01 + a11;
+				if (numer >= denom) {
+					s = 1;
+					t = 0;
+					sqrDistance = a00 + (2) * b0 + c;
+				} else {
+					s = numer / denom;
+					t = 1 - s;
+					sqrDistance = s * (a00 * s + a01 * t + (2) * b0) +
+						t * (a01 * s + a11 * t + (2) * b1) + c;
+				}
+			} else {
+				s = 0;
+				if (tmp1 <= 0) {
+					t = 1;
+					sqrDistance = a11 + (2) * b1 + c;
+				} else if (b1 >= 0) {
+					t = 0;
+					sqrDistance = c;
+				} else {
+					t = -b1 / a11;
+					sqrDistance = b1 * t + c;
+				}
+			}
+		} else if (t < 0)		// region 6
+		{
+			tmp0 = a01 + b1;
+			tmp1 = a00 + b0;
+			if (tmp1 > tmp0) {
+				numer = tmp1 - tmp0;
+				denom = a00 - (2) * a01 + a11;
+				if (numer >= denom) {
+					t = 1;
+					s = 0;
+					sqrDistance = a11 + (2) * b1 + c;
+				} else {
+					t = numer / denom;
+					s = 1 - t;
+					sqrDistance = s * (a00 * s + a01 * t + (2) * b0) +
+						t * (a01 * s + a11 * t + (2) * b1) + c;
+				}
+			}
+
+			else {
+				t = 0;
+				if (tmp1 <= 0) {
+					s = 1;
+					sqrDistance = a00 + (2) * b0 + c;
+				} else if (b0 >= 0) {
+					s = 0;
+					sqrDistance = c;
+				} else {
+					s = -b0 / a00;
+					sqrDistance = b0 * s + c;
+				}
+			}
+		} else				// region 1
+		{
+			numer = a11 + b1 - a01 - b0;
+			if (numer <= 0) {
+				s = 0;
+				t = 1;
+				sqrDistance = a11 + (2) * b1 + c;
+			} else {
+				denom = a00 - (2) * a01 + a11;
+				if (numer >= denom) {
+					s = 1;
+					t = 0;
+					sqrDistance = a00 + (2) * b0 + c;
+				} else {
+					s = numer / denom;
+					t = 1 - s;
+					sqrDistance = s * (a00 * s + a01 * t + (2) * b0) +
+						t * (a01 * s + a11 * t + (2) * b1) + c;
+				}
+			}
+		}
+
+	}
+
+	// Account for numerical round-off error.
+	if (sqrDistance < 0) {
+		sqrDistance = 0;
+	}
+
+	return sqrt(sqrDistance);
+}
+
 #endif
